3b8571461dd50c6ac6290e5ff895be727292df25
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-26 18:33:06 +0530

    api url fixed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     productapi: 'https://localhost:44316/api/Products',
-    productDetailsApi: 'http://localhost/api/ProductDetails/',
+    productDetailsApi: 'https://localhost/api/ProductDetails/',
     imageApiUrl: 'https://localhost:44358/api/Images/'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     productapi: 'http://4.188.111.128/api/products',
-    productDetailsApi: 'https://4.188.111.128/api/ProductDetails',
+    productDetailsApi: 'http://4.188.111.128/api/ProductDetails',
     imageApiUrl: 'http://4.224.70.54/api/images/'
 };
